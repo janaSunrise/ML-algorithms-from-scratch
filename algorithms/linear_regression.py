682e867460c242eb2530f1b165dc9fbbebc5c795
janaSunrise <warriordefenderz@gmail.com>
2021-05-01 11:35:33 +0530

    Added tests to linear regression, with metrics.

--- algorithms/linear_regression.py
+++ algorithms/linear_regression.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
 
 from .base import BaseRegression
 
@@ -11,6 +14,30 @@ class LinearRegression(BaseRegression):
         return np.dot(X, w) + b
 
 
+def mean_squared_error(y_true, y_pred):
+    return np.mean((y_true - y_pred)**2)
+
+
+def r2_score(y_true, y_pred):
+    corr_matrix = np.corrcoef(y_true, y_pred)
+    corr = corr_matrix[0,1]
+    return corr**2
+
+
 # Testing
 if __name__ == "__main__":
-    pass
+    # Prepare the data
+    X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Fit and predict with the regressor
+    regressor = LinearRegression(learning_rate=0.01, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+    
+    # Print the MSE
+    mse = mean_squared_error(y_test, predictions)
+    print(f"Mean squared error: {mse}")
+
+    # Print accuracy
+    print(f"Accuracy: {r2_score(y_test, predictions)}")
