8de09f309aed5a05492be789286257dbaae7e839
janaSunrise <warriordefenderz@gmail.com>
2021-05-02 10:39:28 +0530

    Complete the Adaboost algorithm.

--- algorithms/adaboost.py
+++ algorithms/adaboost.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .base import BaseAlgorithm
+
 
 class DecisionStump:
     def __init__(self):
@@ -19,6 +21,61 @@ class DecisionStump:
             predictions[X_column > self.threshold] = -1
 
         return predictions
+
+
+class Adaboost(BaseAlgorithm):
+    def __init__(self, n_clf=5):
+        self.n_clf = n_clf
+        self.clfs = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        weights = np.full(n_samples, (1 / n_samples))
+        self.clfs = []
+
+        for _ in range(self.n_clf):
+            clf = DecisionStump()
+            min_error = float("inf")
+
+            for feature_i in range(n_features):
+                X_column = X[:, feature_i]
+                thresholds = np.unique(X_column)
+
+                for threshold in thresholds:
+                    p = 1
+
+                    predictions = np.ones(n_samples)
+                    predictions[X_column < threshold] = -1
+
+                    missclassified = weights[y != predictions]
+                    error = sum(missclassified)
+
+                    if error > 0.5:
+                        error = 1 - error
+                        p = -1
+
+                    if error < min_error:
+                        clf.polarity = p
+                        clf.threshold = threshold
+                        clf.feature_idx = feature_i
+                        min_error = error
+
+            EPS = 1e-10
+            clf.alpha = 0.5 * np.log((1.0 - min_error + EPS) / (min_error + EPS))
+
+            predictions = clf.predict(X)
+
+            weights *= np.exp(-clf.alpha * y * predictions)
+            weights /= np.sum(weights)
+
+            self.clfs.append(clf)
+
+    def predict(self, X):
+        clf_preds = [clf.alpha * clf.predict(X) for clf in self.clfs]
+        y_pred = np.sum(clf_preds, axis=0)
+        y_pred = np.sign(y_pred)
+
+        return y_pred
 
 
 if __name__ == "__main__":
