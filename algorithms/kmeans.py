7a43c9aa0b210ed1b8552755fa11fc9dfa15763a
janaSunrise <warriordefenderz@gmail.com>
2021-05-03 10:51:55 +0530

    Start working on KMeans.

---
+++ algorithms/kmeans.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from .base import BaseAlgorithm
+
+
+class KMeans(BaseAlgorithm):
+    def fit(self, X, y):
+        raise NotImplementedError
+
+    def predict(self, X):
+        raise NotImplementedError
+
+
+# Testing
+if __name__ == "__main__":
+    pass
