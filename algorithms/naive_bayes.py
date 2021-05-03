39f2d5082650ea59e559f514083e3d609e32cf40
janaSunrise <warriordefenderz@gmail.com>
2021-05-03 09:51:36 +0530

    Added tests for Naive bayes.

--- algorithms/naive_bayes.py
+++ algorithms/naive_bayes.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn import datasets
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
 
 from .base import BaseAlgorithm
 
@@ -54,4 +57,13 @@ class NaiveBayes(BaseAlgorithm):
 
 # Testing
 if __name__ == "__main__":
-    pass
+    X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    clf = NaiveBayes()
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+
+    print(f"Accuracy: {accuracy_score(predictions, y_test)}")
+    print(f"Report: {classification_report(y_test, predictions)}")
