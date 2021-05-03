ea03996571cdc3c010c33bb30d81c0fa852040c8
janaSunrise <warriordefenderz@gmail.com>
2021-05-03 09:48:33 +0530

    Completed Naive bayes algorithm.

--- algorithms/naive_bayes.py
+++ algorithms/naive_bayes.py
@@ -4,11 +4,52 @@ from .base import BaseAlgorithm
 
 
 class NaiveBayes(BaseAlgorithm):
+    def __init__(self):
+        self._classes = None
+        self._mean = None
+        self._var = None
+        self._priors = None
+
     def fit(self, X, y):
-        raise NotImplementedError
+        n_samples, n_features = X.shape
+        self._classes = np.unique(y)
+        n_classes = len(self._classes)
+
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for idx, c in enumerate(self._classes):
+            X_c = X[y == c]
+            self._mean[idx, :] = X_c.mean(axis=0)
+            self._var[idx, :] = X_c.var(axis=0)
+            self._priors[idx] = X_c.shape[0] / float(n_samples)
 
     def predict(self, X):
-        raise NotImplementedError
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        posteriors = []
+
+        for idx, c in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+
+            posterior = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + posterior
+
+            posteriors.append(posterior)
+
+        return self._classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+
+        numerator = np.exp(- (x - mean) ** 2 / (2 * var))
+        denominator = np.sqrt(2 * np.pi * var)
+
+        return numerator / denominator
 
 
 # Testing
